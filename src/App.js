04dc99ecbf02ce4d98ae3fba3b6bc655e63ec34f
93ToXic93 <122994130+93ToXic93@users.noreply.github.com>
2023-08-30 21:56:13 +0300

    trying to commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div className="content">
-      <h1>hello!</h1>
+      <h1>hello! thats my first comit!</h1>
       </div>
     </div>
   );
