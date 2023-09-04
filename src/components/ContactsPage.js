87febfd658fb3c5dfad6e3ad547637c6921f79f4
93ToXic93 <122994130+93ToXic93@users.noreply.github.com>
2023-09-04 11:46:33 +0300

    Trying to make my website to work with links

---
+++ src/components/ContactsPage.js
@@ -0,0 +1,13 @@
+
+
+function ContactsPage() {
+    return (
+      <div>
+        <h1>Here are my contacts</h1>
+        <h2>Contact 1</h2>
+        <h2>Contact 2</h2>
+      </div>
+    );
+  }
+
+  export default ContactsPage;
